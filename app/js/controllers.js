2e814e510325b3b599a8c4b76b829c6657e566df
Wil Black <wilblack21@gmail.com>
2014-04-28 01:30:05 -0700

    Got target mode working, and changes to scan to passive automatically.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('myApp.controllers', [])
     var timer_dir = -1;
     $scope.app = {bots:['ctenophore']};  // Bots to control
     $scope.tentacle = {leds:[], active_index:0, mode:"passive"};
-    $scope.dt = 1000;
+    $scope.dt = 400;
 
     var ledBase = {index:0,
                    color:"#000000",
@@ -24,22 +24,32 @@ angular.module('myApp.controllers', [])
     
 
     $scope.setMode = function(mode){
+        // Cancel any previous modes
         if (angular.isDefined(timer)) $scope.cancelTimer(timer);
         
-        var message = {command:"setMode", 
-                       kwargs:{mode:mode}
-                      };
+        $scope.allOff();
+        
 
-        ardyhWs.send(message);
         if (mode === 'scan'){
+            var NTIMES = 2*NUM_LEDS;
+            var count = 0;
             timer = $interval(function() {
-                if( ($scope.tentacle.active_index === (NUM_LEDS-1)) || ($scope.tentacle.active_index === 0 )) {
+                count++;
+                if (count === NTIMES) {
+                    $scope.tentacle.mode = 'passive';
+                } else {
+                    if( ($scope.tentacle.active_index === (NUM_LEDS-1)) || ($scope.tentacle.active_index === 0 )) {
                     timer_dir = ($scope.tentacle.active_index === 0)? 1 : -1;
-                } 
-                $scope.tentacle.active_index = ($scope.tentacle.active_index + timer_dir);
-                $scope.tentacle.leds[$scope.tentacle.active_index].color = "#FF0000";
-                $scope.tentacle.leds[$scope.tentacle.active_index-timer_dir].color = "#000000";
-            }, $scope.dt);
+                    } 
+                    $scope.tentacle.active_index = ($scope.tentacle.active_index + timer_dir);
+                    $scope.tentacle.leds[$scope.tentacle.active_index].color = "#FF0000";
+                    $scope.tentacle.leds[$scope.tentacle.active_index-timer_dir].color = "#000000";
+                }
+
+            }, 0.5*$scope.dt, NTIMES);
+
+            
+        
         } else if (mode === 'passive'){
             timer = $interval(function() {
                 var old_index = $scope.tentacle.active_index
@@ -48,9 +58,55 @@ angular.module('myApp.controllers', [])
                 $scope.tentacle.leds[$scope.tentacle.active_index].color = ("#"+(Math.random().toString(16) + '000000').slice(2, 8));
                 $scope.tentacle.leds[old_index].color = "#000000";
             }, $scope.dt);
+
+        } else if (mode === "target"){
+            var width = 6;
+            var dt = 0.2;
+            var lefti, righti, message;
+            var NTIMES = 25;
+            var count = 0;            
+
+            var i = width;
+
+            timer = $interval(function(){
+                count++;  
+                if (count === NTIMES){ // If this is the last time through set the mode to scan.
+                    $scope.tentacle.mode = 'scan';
+                } else {
+                    if (i < 0 ) i=width;
+                
+                    
+                    $scope.allOff();
+
+                    lefti = $scope.tentacle.active_index - i;
+                    righti= $scope.tentacle.active_index + i;
+                    
+                    message =  {command:"setRGB"};
+                    if (righti < NUM_LEDS-1){
+                        message.kwargs = {
+                            index:righti,
+                            color : "#FF0000"
+                        };
+                        ardyhWs.send(message);
+                        $scope.tentacle.leds[righti].color = "#FF0000";
+                    }
+                    
+                    if (lefti > 0){
+                        message.kwargs = {
+                            index:lefti,
+                            color : "#FF0000"
+                        };
+                        ardyhWs.send(message);
+                        $scope.tentacle.leds[lefti].color = "#FF0000";
+                    }
+
+                    i--;
+                }
+                
+            }, 0.5*$scope.dt, NTIMES);
+
         } else if (mode === "off"){
-            message = {command:"allOff"}
-            ardyhWs.send(message);
+            $scope.allOff();
         }
     };
 
@@ -60,6 +116,15 @@ angular.module('myApp.controllers', [])
         for (var i=0; i < NUM_LEDS; i++){
             $scope.tentacle.leds[i].color = '#000000';
         }
+    };
+
+    $scope.allOff = function(){
+        var message = {command:"allOff", kwargs:{} };
+        ardyhWs.send(message);
+        
+        _.each($scope.tentacle.leds, function(led){
+            led.color = "#000000";
+        });
     };
 
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -12,6 +12,15 @@ var app = angular.module('myApp.directives', []).
 
 
 app.directive('ledcolor', ['ardyhWsFactory', function(ardyhWs){
+    
+
+    function ledClickCallback(event){
+        console.log("clicked led");
+        console.log(event);
+
+        // Set target algorithm here
+    }
+
     return {
         restruct:'A',
         scope : true,
@@ -19,7 +28,6 @@ app.directive('ledcolor', ['ardyhWsFactory', function(ardyhWs){
         link : function(scope, element, attrs){
 
             scope.$watch('led.color', function(newValue){
-                console.log("led "+scope.led.index+" changed to "+newValue);
                 // Make command, color shold be a HEX color string (with the hash)
                 var message = {command:"setRGB", 
                                kwargs:{color:scope.led.color, index:scope.led.index}
@@ -27,7 +35,16 @@ app.directive('ledcolor', ['ardyhWsFactory', function(ardyhWs){
 
                 ardyhWs.send(message);
             });
+
+            element.on('click', function(event){
+                console.log("LED clicked index " + scope.led.index);
+                scope.tentacle.active_index = scope.led.index;
+                scope.tentacle.mode = 'target';
+            });
         }
+
+
+
     };
 }]);
 
